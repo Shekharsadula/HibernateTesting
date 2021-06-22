76328422eabce9f182321aa619447ccb296c9e09
ADMIN <shekharsadula@gmail.com>
2021-06-22 10:52:57 +0530

    added cases msg in comment in apptest.java

--- src/test/java/com/ss/HibernateTesting/AppTest.java
+++ src/test/java/com/ss/HibernateTesting/AppTest.java
@@ -13,7 +13,7 @@ public class AppTest
     /**
      * Create the test case
      *
-     * @param testName name of the test 
+     * @param testName name of the test cases
      */
     public AppTest( String testName )
     {
